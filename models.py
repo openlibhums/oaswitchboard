e55f0368c4f84add613c2d7d813146db36659203
Martin Paul Eve <martin@eve.gd>
2024-12-02 16:23:14 +0000

    Initial model

---
+++ models.py
@@ -0,0 +1,25 @@
+"""
+Models for the OAS plugin.
+"""
+
+from django.db import models
+
+
+class SwitchboardMessage(models.Model):
+    """
+    A message that has been sent to the switchboard.
+    """
+
+    broadcast = models.BooleanField(default=True)
+    message_type = models.CharField(max_length=255, default="p1-pio")
+    authorized = models.BooleanField(default=False)
+
+    article = models.OneToOneField(
+        "submission.Article",
+        on_delete=models.CASCADE,
+    )
+
+    message = models.TextField()
+    response = models.TextField()
+
+    message_date_time = models.DateTimeField(auto_now_add=True)
